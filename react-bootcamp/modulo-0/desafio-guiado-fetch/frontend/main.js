8b562ba8f61913d00f511865fa9572e1a73aa6f7
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2022-01-29 14:32:43 -0300

    Criando Elementos por demanda

--- react-bootcamp/modulo-0/desafio-guiado-fetch/frontend/main.js
+++ react-bootcamp/modulo-0/desafio-guiado-fetch/frontend/main.js
@@ -17,15 +17,21 @@ async function init() {
 init();
 
 function renderData() {
-  let items = employees.map((employee) => {
+  for (const employee of employees) {
     let role = roles.find((role) => role.id == employee.role_id);
-    return `<li>${employee.name}<br>${role.name}</li>`;
-  });
-  listEl.innerHTML = items.join("");
+    const li = document.createElement("li");
+    const divName = document.createElement("div");
+    divName.textContent = employee.name;
+    const divRole = document.createElement("div");
+    divRole.textContent = role.name;
+    li.appendChild(divName);
+    li.appendChild(divRole);
+    listEl.appendChild(li);
+  }
 }
 
 function showError(error) {
-  document.getElementById("app").innerHTML = "Erro ao carregar dados.";
+  document.getElementById("errors").textContent = "Erro ao carregar dados.";
   console.error(error);
 }
 
