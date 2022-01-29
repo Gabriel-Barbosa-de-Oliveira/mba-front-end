72bd24abf7d6aef1d3285061b774c0a16797c546
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2022-01-29 14:27:36 -0300

    Renderizando Lista

---
+++ react-bootcamp/modulo-0/desafio-guiado-fetch/frontend/main.js
@@ -0,0 +1,51 @@
+let employees = [];
+let roles = [];
+const listEl = document.querySelector('ul');
+const formEl = document.querySelector('form');
+
+async function init() {
+  try {
+    [employees, roles] = await Promise.all([
+      listEmployees(),
+      listRoles()
+    ]);
+    renderData();
+  } catch (erro) {
+    showError(erro);
+  }
+}
+init();
+
+function renderData() {
+  let items = employees.map((employee) => {
+    let role = roles.find((role) => role.id == employee.role_id);
+    return `<li>${employee.name}<br>${role.name}</li>`;
+  });
+  listEl.innerHTML = items.join("");
+}
+
+function showError(error) {
+  document.getElementById("app").innerHTML = "Erro ao carregar dados.";
+  console.error(error);
+}
+
+/*
+// Criar
+fetch(`http://localhost:3000/employees`, {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify(employee),
+});
+
+// Atualizar
+fetch(`http://localhost:3000/employees/${id}`, {
+  method: "PUT",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify(employee),
+});
+
+// Excluir
+fetch(`http://localhost:3000/employees/${id}`, {
+  method: "DELETE",
+});
+*/
