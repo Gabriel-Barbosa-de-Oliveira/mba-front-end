1a80fc9aa7fa621eddb26824fb1ca260baf5f05d
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2022-02-11 16:34:41 -0300

    Setando um novo valor para um input. Maneira certa

--- react-bootcamp/modulo-2-react/react-hello/src/App.js
+++ react-bootcamp/modulo-2-react/react-hello/src/App.js
@@ -1,9 +1,22 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Test from "./components/Test";
 
 export default function App() {
 
+  // const state = useState('Gabriel');
+  // const name = state[0];
+  // const setName = state[1];
+
+  const [name, setName] = useState('Gabriel');
+
+  //Clojure
+
+  function handleNameChange(event) {
+    const newName = event.currentTarget.value;
+    setName(newName)
+  }
 
   return (
     <>
@@ -11,9 +24,11 @@ export default function App() {
         Componente Header - projeto react-hello
       </Header>
       <Main>
-        <label htmlFor="inputName">Digite seu nome: </label>
-        <input id="inputName" className="border" type='text' />
-        <p>O seu nome é Gabriel e você possui 24 anos.</p>
+        <div className="flex flex-col my-4">
+          <label htmlFor="inputName" className="text-sm mb-1">Digite seu nome: </label>
+          <input autoFocus id="inputName" className="border p-1" type='text' value={name} onChange={handleNameChange} />
+        </div>
+        <p>O seu nome é {name}, com {name.length} caracteres, e você possui 24 anos.</p>
       </Main>
     </>
   )
