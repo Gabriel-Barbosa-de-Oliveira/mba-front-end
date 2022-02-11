790b0aea62e14f6cafa8300b33d94c6f81001bce
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2022-02-11 16:18:45 -0300

    Como mandar props e seus tipos

--- react-bootcamp/modulo-2-react/react-hello/src/App.js
+++ react-bootcamp/modulo-2-react/react-hello/src/App.js
@@ -1,23 +1,20 @@
 import Header from "./components/Header";
+import Main from "./components/Main";
+import Test from "./components/Test";
 
 export default function App() {
-  console.log('Teste no console do navegador')
-
-  const name = "Gabriel";
-  const age = 24;
 
   return (
     <>
       <Header size="large">
         Componente Header - projeto react-hello
       </Header>
-
-
-      <main>
-        <div className="container mx-auto p-4">
-          <p>O seu nome é {name} e você possui {age} anos.</p>
-        </div>
-      </main>
+      <Main>
+        <p>O seu nome é Gabriel e você possui 24 anos.</p>
+      </Main>
+      <Test number={10} string="Teste" visible data={{ a: 1, b: 2 }} onClick={() => {
+        console.log('click')
+      }}></Test>
     </>
   )
 }
---
+++ react-bootcamp/modulo-2-react/react-hello/src/components/Test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Test(props) {
+  console.log(props)
+  return (
+    <div>Test</div>
+  )
+}
+
+export default Test
