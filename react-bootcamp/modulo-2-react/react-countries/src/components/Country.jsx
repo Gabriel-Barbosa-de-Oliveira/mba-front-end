cc4adb6a34bb6bee418aef842c8a523ab1cc3575
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2022-02-13 14:09:05 -0300

    Renderizando países

--- react-bootcamp/modulo-2-react/react-countries/src/components/Country.jsx
+++ react-bootcamp/modulo-2-react/react-countries/src/components/Country.jsx
@@ -1,8 +1,37 @@
 import React from "react";
+import Item from "./Item";
 
 export default function Country({ children: country = null }) {
   if (!country) {
     return <div>Impossível renderizar o páis</div>;
   }
-  return <div>{country.name}</div>;
+
+  const { flag, name, capital, region, population, area } = country;
+  const demographicDensity = population / area;
+
+  return (
+    <div className="border p-2 m-2 flex flex-row items-center space-x-2">
+      <img className="w-48" src={flag} alt={name} />
+      <ul>
+        <li>
+          <Item label="Nome:">{name}</Item>
+        </li>
+        <li>
+          <Item label="Capital:">{capital}</Item>
+        </li>
+        <li>
+          <Item label="Região:">{region}</Item>
+        </li>
+        <li>
+          <Item label="População:">{population}</Item>
+        </li>
+        <li>
+          <Item label="Area:">{area}</Item>
+        </li>
+        <li>
+          <Item label="Densidade Demográfica:">{demographicDensity}</Item>
+        </li>
+      </ul>
+    </div>
+  );
 }
