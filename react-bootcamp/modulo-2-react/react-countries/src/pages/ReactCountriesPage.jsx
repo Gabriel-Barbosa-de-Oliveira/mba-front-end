b9d8a509ef982274ff9aabcd785caf0d23f0833a
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2022-02-13 14:36:56 -0300

    Prop Drilling Removido - Composition implementado

--- react-bootcamp/modulo-2-react/react-countries/src/pages/ReactCountriesPage.jsx
+++ react-bootcamp/modulo-2-react/react-countries/src/pages/ReactCountriesPage.jsx
@@ -4,6 +4,7 @@ import TextInput from "../components/TextInput";
 import Header from "../components/Header";
 import { allCountries } from "../data/countries";
 import Countries from "../components/Countries";
+import Country from "../components/Country";
 export default function ReactCountriesPage() {
   const [countryFilter, setCountryFilter] = useState("");
   const [visitedCountries, setVisitedCountries] = useState([]);
@@ -48,11 +49,27 @@ export default function ReactCountriesPage() {
           id="inputCountryFilter"
           onInputChange={handleCountryFilterChange}
         />
-        <Countries
-          visitedCountries={visitedCountries}
-          onCountryClick={toggleVisitedCountry}
-        >
-          {filteredCountries}
+
+        <Countries>
+          <h2 className="text-center font-semibold">
+            {filteredCountries.length} país(es)
+          </h2>
+          <h3 className="text-center font-semibold text-sm">
+            {visitedCountries.length} país(es) visitados
+          </h3>
+          {filteredCountries.map((country) => {
+            const isVisited = visitedCountries.indexOf(country.id) !== -1;
+
+            return (
+              <Country
+                isVisited={isVisited}
+                onCountryClick={toggleVisitedCountry}
+                key={country.id}
+              >
+                {country}
+              </Country>
+            );
+          })}
         </Countries>
       </Main>
     </div>
