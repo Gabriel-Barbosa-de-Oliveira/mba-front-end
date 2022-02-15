4c650ac7816a74a862903d7a8dedfba8c58b094a
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2022-02-15 18:38:48 -0300

    Radio Buttons

--- react-bootcamp/modulo-2-react/react-flash-cards-v1/src/pages/FlashCardPage.jsx
+++ react-bootcamp/modulo-2-react/react-flash-cards-v1/src/pages/FlashCardPage.jsx
@@ -4,14 +4,24 @@ import FlashCard from "../components/FlashCard";
 import FlashCards from "../components/FlashCards";
 import Header from "../components/Header";
 import Main from "../components/Main";
+import RadioButton from "../components/RadioButton";
 import { allFlashCards } from "../data/flashcards";
 import { helperShuffleArray } from "../helpers/arrayHelpers";
 export default function FlashCardPage() {
   const [allCards, setAllCards] = useState(allFlashCards);
+  const [showTitle, setShowTitle] = useState(true);
 
   function handleButtonClick() {
     const shuffleCards = helperShuffleArray(allCards);
     setAllCards(shuffleCards);
+  }
+
+  function handleRadioShowDescriptionClick() {
+    setShowTitle(false);
+  }
+
+  function handleRadioShowTitleClick() {
+    setShowTitle(true);
   }
 
   return (
@@ -20,6 +30,24 @@ export default function FlashCardPage() {
       <Main>
         <div className="text-center mb-4">
           <Button onButtonClick={handleButtonClick}>Embaralhar Cards</Button>
+        </div>
+        <div className="flex flex-row items-center justify-center space-x-4">
+          <RadioButton
+            id="radioButtonShowTitle"
+            name="showInfo"
+            buttonChecked={showTitle}
+            onButtonClick={handleRadioShowTitleClick}
+          >
+            Mostrar Título
+          </RadioButton>
+          <RadioButton
+            id="radioButtonShowDescription"
+            name="showInfo"
+            buttonChecked={!showTitle}
+            onButtonClick={handleRadioShowDescriptionClick}
+          >
+            Mostrar Descrição
+          </RadioButton>
         </div>
         <FlashCards>
           {allCards.map(({ id, title, description }) => {
