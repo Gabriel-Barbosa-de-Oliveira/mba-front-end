15a1d7680088e939a48f496a3bc2e7171017ba63
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2022-02-15 17:49:06 -0300

    Embaralhando Cards

--- react-bootcamp/modulo-2-react/react-flash-cards-v1/src/pages/FlashCardPage.jsx
+++ react-bootcamp/modulo-2-react/react-flash-cards-v1/src/pages/FlashCardPage.jsx
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "../components/Button";
 import FlashCard from "../components/FlashCard";
 import FlashCards from "../components/FlashCards";
 import Header from "../components/Header";
 import Main from "../components/Main";
 import { allFlashCards } from "../data/flashcards";
+import { helperShuffleArray } from "../helpers/arrayHelpers";
 export default function FlashCardPage() {
-  console.log(allFlashCards);
+  const [allCards, setAllCards] = useState(allFlashCards);
+
+  function handleButtonClick() {
+    const shuffleCards = helperShuffleArray(allCards);
+    setAllCards(shuffleCards);
+  }
+
   return (
     <>
       <Header>react-flash-cards-v1</Header>
       <Main>
+        <div className="text-center mb-4">
+          <Button onButtonClick={handleButtonClick}>Embaralhar Cards</Button>
+        </div>
         <FlashCards>
-          {allFlashCards.map(({ id, title, description }) => {
+          {allCards.map(({ id, title, description }) => {
             return (
               <FlashCard key={id} title={title} description={description} />
             );
