aa001b180dd174616cf06e08dffd77e51593da0d
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2022-02-19 12:34:25 -0300

    Estrutura inicial

---
+++ react-bootcamp/modulo-2-react/desafio/src/pages/ReactElections.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
+import Main from "../components/Main";
+
+export default function ReactElections() {
+  return (
+    <div>
+      <Header>react-elections</Header>
+
+      <Main>Conteudo</Main>
+    </div>
+  );
+}
