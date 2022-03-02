13d699918b4abefb096c07f2f0c31d6b87fbe418
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2022-03-02 20:01:48 -0300

    Fazendo chamadas iniciais + backend + install de libraries

--- react-bootcamp/modulo-3-react/agenda-react/src/interfaces/backend.tsx
+++ react-bootcamp/modulo-3-react/agenda-react/src/interfaces/backend.tsx
@@ -11,3 +11,14 @@ export interface IEvent {
   desc: string;
   calendarId: number;
 }
+
+export function getCalendarsEndpoint(): Promise<Array<ICalendar>> {
+  return fetch("http://localhost:8080/calendars").then((resp) => {
+    return resp.json();
+  });
+}
+export function getEventsEndpoint(): Promise<Array<IEvent>> {
+  return fetch("http://localhost:8080/events").then((resp) => {
+    return resp.json();
+  });
+}
