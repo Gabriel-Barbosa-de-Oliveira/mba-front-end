8f940c6f11feac57a1e829747554aa3e208f4686
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2022-03-19 11:53:56 -0300

    Introdução ao Styled Buttons

--- react-bootcamp/modulo-4-react/aula-styled-components/src/App.tsx
+++ react-bootcamp/modulo-4-react/aula-styled-components/src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import styled from "styled-components";
+
+const StyleHeader = styled.h1`
+  color: #3636e9;
+  font-size: 42px;
+`;
+
+const StyleData = styled("p")`
+  font-size: 24px;
+  font-weight: 700;
+  color: #8d8dff;
+`;
+
+const StyleButton = styled.button`
+  background-color: transparent;
+  border-radius: 8px;
+  border: 1px solid black;
+  padding: 8px 16px;
+  cursor: pointer;
+`;
+
+const StyleSuccessButton = styled(StyleButton)`
+  border-color: green;
+  color: green;
+`;
+
+const StyleFailedButton = styled(StyleButton)`
+  border-color: red;
+  color: red;
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <StyleHeader>Gabriel Barbosa de Oliveira</StyleHeader>
+      <StyleData>barbosagabrieloliveira@hotmail.com</StyleData>
+      <StyleData>(12) 99239-9504</StyleData>
+      <StyleData>Brasil</StyleData>
+
+      <StyleSuccessButton>Adicionar</StyleSuccessButton>
+      <StyleFailedButton>Remover</StyleFailedButton>
+      <StyleButton>Detalhes</StyleButton>
     </div>
   );
 }
