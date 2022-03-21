5427033154e361417a65f72132437d01a4662b80
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2022-03-21 18:31:10 -0300

    Roteamento por Link

--- react-bootcamp/modulo-4-react/aula-next/pages/examples/dynamic/[name].tsx
+++
@@ -1,9 +0,0 @@
-import { useRouter } from "next/router";
-
-const Dynamic = () => {
-  const router = useRouter();
-  const { name } = router.query;
-  return <div>I'm a dynamic route and I was called with: {name}</div>;
-};
-
-export default Dynamic;
