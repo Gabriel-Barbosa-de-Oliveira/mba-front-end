78a73bcc967e50bcf94eb3ed11d18862f92c70b1
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2022-03-21 21:11:13 -0300

    Index Funcionando e estilizado

--- react-bootcamp/modulo-4-react/trabalho/pages/store/index.tsx
+++ react-bootcamp/modulo-4-react/trabalho/pages/store/index.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import styled, { css, keyframes, ThemeProvider } from "styled-components";
+import { useState } from "react";
 
 export async function getStaticProps() {
   const products = await fetch(
@@ -15,6 +17,46 @@ export async function getStaticProps() {
   };
 }
 
+const StyledContainer = styled.section`
+  display: grid;
+  grid-template-rows: auto 1fr;
+  grid-template-columns: 1fr;
+  display: grid;
+  justify-items: center;
+  width: 100%;
+  height: 100%;
+  background-color: #f9f9f9;
+`;
+
+const StyleHeader = styled.h1`
+  color: black;
+  font-size: 42px;
+  text-align: center;
+  padding: 16px;
+  font-weight: 400;
+`;
+
+const StyledList = styled.ul`
+  list-style: none;
+  padding: 0;
+  width: 100%;
+`;
+
+const StyledLi = styled.li`
+  padding: 16px;
+  font-size: 20px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+`;
+
+const StyledImg = styled.img`
+  justify-self: end;
+  align-self: center;
+  margin-right: 32px;
+  height: 40px;
+  width: 40px;
+`;
+
 const Index = ({
   products,
   lastRenderer,
@@ -23,20 +65,21 @@ const Index = ({
   lastRenderer: Date;
 }) => {
   return (
-    <div>
-      <div>{lastRenderer}</div>
-      <ul>
+    <StyledContainer>
+      <StyleHeader>Loja de Hardware</StyleHeader>
+      <StyledList>
         {products.map((product) => {
           return (
-            <li key={product.id}>
+            <StyledLi key={product.id}>
               <Link href={`/store/${product.id}`}>
                 <a>{product.title}</a>
               </Link>
-            </li>
+              <StyledImg src={product.image} />
+            </StyledLi>
           );
         })}
-      </ul>
-    </div>
+      </StyledList>
+    </StyledContainer>
   );
 };
 
