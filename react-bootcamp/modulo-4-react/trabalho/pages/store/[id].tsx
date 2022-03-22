7adc88882200a02b44cf1775e0f9be58656aa1d5
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2022-03-21 21:16:38 -0300

    Produto

--- react-bootcamp/modulo-4-react/trabalho/pages/store/[id].tsx
+++ react-bootcamp/modulo-4-react/trabalho/pages/store/[id].tsx
@@ -1,3 +1,4 @@
+import styled, { css, keyframes, ThemeProvider } from "styled-components";
 export async function getStaticPaths() {
   const products = await fetch(
     `https://fakestoreapi.com/products?sort=desc&limit=10`
@@ -9,7 +10,7 @@ export async function getStaticPaths() {
 
   return {
     paths,
-    fallback: true,
+    fallback: false,
   };
 }
 export async function getStaticProps({ params }: { params: any }) {
@@ -24,17 +25,41 @@ export async function getStaticProps({ params }: { params: any }) {
   };
 }
 
+const StyledContainer = styled.section`
+  display: grid;
+  grid-template-rows: auto 1fr;
+  grid-template-columns: 1fr;
+  display: grid;
+  justify-items: center;
+  width: 100%;
+  height: 100%;
+  background-color: #f9f9f9;
+`;
+
+const StyleHeader = styled.h1`
+  color: black;
+  font-size: 42px;
+  text-align: center;
+  padding: 16px;
+  font-weight: 400;
+`;
+
+const StyledImg = styled.img`
+  align-self: start;
+  max-height: 400px;
+  max-width: 400px;
+`;
+
 const Product = ({ product }: { product: any }) => {
   if (!product) {
     return <div></div>;
   }
   return (
-    <div>
-      <span>
-        {product.title}
-        {product.price}
-      </span>
-    </div>
+    <StyledContainer>
+      <StyleHeader>{product.title}</StyleHeader>
+      <StyledImg src={product.image} />
+      {product.price}
+    </StyledContainer>
   );
 };
 
