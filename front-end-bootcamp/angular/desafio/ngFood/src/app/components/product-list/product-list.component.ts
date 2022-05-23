f8267fc76985781a79ff2f1b396ad627549fa34c
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2022-05-23 19:33:38 -0300

    Product list view

--- front-end-bootcamp/angular/desafio/ngFood/src/app/components/product-list/product-list.component.ts
+++ front-end-bootcamp/angular/desafio/ngFood/src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CurrencyService } from 'src/app/services/currency.service';
 import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
@@ -11,7 +12,7 @@ export class ProductListComponent implements OnInit {
   public products: any = {};
   Object = Object;
 
-  constructor(private productService: ProductsService) {}
+  constructor(private productService: ProductsService, private currencyService: CurrencyService) {}
 
   ngOnInit(): void {
     this.getProductsFromService();
@@ -25,6 +26,11 @@ export class ProductListComponent implements OnInit {
   public watchForProductRequest(){
     this.productService.formattedBackendList.subscribe(products => {
       this.products = products;
+      console.log(this.products)
     })
+  }
+
+  public convertValueToCurrencyStandard(value: number){
+    return this.currencyService.convertoToSpecifiedCurrency(value);
   }
 }
--- front-end-bootcamp/angular/desafio/ngFood/src/app/services/products.service.ts
+++ front-end-bootcamp/angular/desafio/ngFood/src/app/services/products.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { BackendService } from './backend.service';
 import * as _ from "lodash";
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable({
   providedIn: 'root',
@@ -15,12 +16,19 @@ export class ProductsService {
   public getProducts(): void {
     this.backendService.getFoodList().subscribe(
       (products) => {
-        this.products.next(products);
-        this.formattedBackendList.next(_.groupBy(products, 'categoria'));
+        const formattedProducts = this.mapProductId(products)
+        this.products.next(formattedProducts);
+        this.formattedBackendList.next(_.groupBy(formattedProducts, 'categoria'));
       },
       (error) => {
         console.log(error);
       }
     );
+  }
+
+  private mapProductId(products: Array<any>): Array<any>{
+    return products.map(product => {
+      return ({ ...product, id: uuidv4() })
+    });
   }
 }
